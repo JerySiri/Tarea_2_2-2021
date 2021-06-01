d1c820793e19623b589fc331fa171e940c292a2b
JerySiri <jeryjavier@hotmail.com>
2021-06-01 02:29:26 -0400

    Arreglos en la validacion de los registros. Closes #2.

--- Models/Prestamos.cs
+++ Models/Prestamos.cs
@@ -13,10 +13,10 @@ namespace Tarea_2_2_2021.Models
 
         public DateTime FechaCreacion { get; set; } = DateTime.Now;
 
-        [Required(ErrorMessage = "Es obligatorio elegir un Usuario")]
-        public int UsuarioId { get; set; }
+        [Required(ErrorMessage = "Es obligatorio elegir un Usuario.")]
+        public int? UsuarioId { get; set; }
 
-        [Required(ErrorMessage = "Es obligatorio introducir el Concepto")]
+        [Required(ErrorMessage = "Es obligatorio introducir el Concepto.")]
         public string Concepto { get; set; }
 
         public float Balance { get; set; } = 0;
--- Models/Usuarios.cs
+++ Models/Usuarios.cs
@@ -8,16 +8,19 @@ namespace Tarea_2_2_2021.Models
     	[Key]
     	public int UsuarioId { get; set; }
 
-    	[Required(ErrorMessage ="Es obligatorio introducir el nombre")]
+        [Required(ErrorMessage ="Es obligatorio introducir el nombre.")]
+        [MinLength(4, ErrorMessage = "El Nombre debe tener por lo menos 4 caracteres.")]
         public string Nombre { get; set; }
         
     	[Required(ErrorMessage ="Es obligatorio introducir el telefono")]
-    	public string Telefono { get; set; }
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$",ErrorMessage = "Introdusca el formato valido de numero de Teléfono Ejemplo: 000-000-0000")]
+        public string Telefono { get; set; }
 
     	[Required(ErrorMessage ="Es obligatorio introducir el cedula")]
-    	public string Cedula { get; set; }
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{7})[-. ]?([0-9]{1})$", ErrorMessage = "Introdusca el formato valido de la Cédula Ejemplo: 000-0000000-0")]
+        public string Cedula { get; set; }
 
-    	[Required(ErrorMessage ="Es obligatorio introducir el dirección")]
+    	[Required(ErrorMessage ="Es obligatorio introducir el dirección.")]
     	public string Direccion {get; set; }
 
         public DateTime FechaNacimiento { get; set; } = DateTime.Now;
